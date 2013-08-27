a585b2c48ac3f065cf407a26a288e2b2599d06ac
Caleb <calebbradley30@yahoo.com>
2013-08-26 22:06:11 -0400

    ArrayPractice

---
+++ array.js
@@ -0,0 +1,29 @@
+array.js
+
+
+var carMakers = [
+    { name: 'Chevy', models: [
+    		{ name: 'Corvette', features: ['2dr', 'convertable'] },
+    		{ name: 'Silverado', features: ['3dr', '4dr'] },
+    		{ name: 'Tahoe', features: ['standard', '3rd-row'] }
+    	]},
+
+    { name: 'Toyota', models: [
+    		{ name: 'Prius', features: ['green', 'superGreen'] },
+    		{ name: 'Camry', features: ['economy', '4dr'] },
+    		{ name: 'Corolla', features: ['2dr', '4dr'] }
+    	]}
+    { name: 'Ford', models: [
+    		{ name: 'Mustang', features: ['2dr', 'convertable'] },
+    		{ name: 'Explorer', features: ['2dr', '4dr'] },
+    		{ name: 'F-Series', features: ['150', '250', '350'] }
+    	]},
+
+    { name: 'Dodge', models: [
+    		{ name: 'Stratus', features: ['2dr', '4dr'] },
+    		{ name: 'Durango', features: ['4dr'] },
+    		{ name: 'Ram', features: ['1500', '2500', '3500'] }
+    	]}
+];
+
+
